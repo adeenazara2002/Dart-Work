8c9b11fcc825b853eb0e05b9741f6ea2c0d83475
adeenazara2002 <adinazara2001@gmail.com>
2024-03-16 23:28:52 +0500

    Updating Task 01 and Task 03

--- Assignment_04/task_01.dart
+++ Assignment_04/task_01.dart
@@ -1,10 +1,10 @@
 void main() {
- List names = ['Adina' , 'Tabassum' , 'Myra' , 'Saad' , 'Usman' , 'Aswa'];
-  print(names[0]);
-  print(names[1]);
-  print(names[2]);
-  print(names[3]);
-  print(names[4]);
-  print(names[5]);
-  
-  }
+  List names = ['Adina', 'Tabassum', 'Myra', 'Saad', 'Usman', 'Aswa'];
+  // print(names[0]);
+  // print(names[1]);
+  // print(names[2]);
+  // print(names[3]);
+  // print(names[4]);
+  // print(names[5]);
+  print(names);
+}
--- Assignment_04/task_03.dart
+++ Assignment_04/task_03.dart
@@ -1,15 +1,22 @@
-void main(){
- List days = ['Monday' , 'Tuesday' , 'Wednesday' , 'Thrusday' , 'Friday' , 'Saturday' , 'Sunday'];
+void main() {
+  List days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thrusday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
   print("Before removing elements");
   print(days);
   print("After removing elements");
- print(days.remove(0));
- print(days.remove(1));
- print(days.remove(2));
- print(days.remove(3));
- print(days.remove(4));
- print(days.remove(5));
- print(days.remove(6));
-  
-  
+  print(days.removeLast());
+  print(days.removeLast());
+  print(days.removeLast());
+  print(days.removeLast());
+  print(days.removeLast());
+  print(days.removeLast());
+  print(days.removeLast());
+
 }
