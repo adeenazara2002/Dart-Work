85a90b9a9379909c379edc2c074dd442faa93315
adeenazara2002 <adinazara2001@gmail.com>
2024-03-16 23:54:11 +0500

    Updating task 04

--- Assignment_04/task_04.dart
+++ Assignment_04/task_04.dart
@@ -1,9 +1,9 @@
 void main() {
-  List<int> numbers = [23, 445, 12, 2, 99, 100, 156, 10];
-  
-  int minNum = numbers.reduce((currMin, num) => num < currMin ? num : currMin);
-  int maxNum = numbers.reduce((currMax, num) => num > currMax ? num : currMax);
-  
-  print('Smallest number: $minNum');
-  print('Greatest number: $maxNum');
+  List numbers = [567, 34, 21, 1, 99, 56, 100, 1000];
+  print(numbers);
+
+  numbers.sort();
+  print("The smallest number in the list is ${numbers[0]}");
+  print("The greatest number in the list is ${numbers[7]}");
+
 }
