f14b618521f4268b62a60bc662389b2f663f44b6
adeenazara2002 <adinazara2001@gmail.com>
2024-03-23 20:13:13 +0500

    Map Methods

---
+++ flutter_practise/mapMethods.dart
@@ -0,0 +1,23 @@
+void main(){
+  Map<int,String> test = {1:"chemistry" , 2: "biology" ,3: "Maths" , 4: "Computer Science"};
+  // by default dynamic
+
+//  test.clear();
+// print("This is my map $test");
+// test.addAll({3:"Matric" , 1: "chemistry"});  update
+// print(test.isEmpty);
+// test[4] = "Physics";
+// print(test);
+// test.putIfAbsent(4, () => "maths");  error function
+// test.removeWhere((key, value) => key == 1);
+// print(test);
+// print(test[2]);
+
+}
+
+
+
+
+// to access a particular key in maps in lists 
+// print(abc[0]["userId"]);
+// syntax for add all ... spread operators performs addition
