20ca56a55676d43628292923ec0f9b932f158b95
adeenazara2002 <adinazara2001@gmail.com>
2024-04-30 09:40:49 +0500

    Update Changes and adding Task 01 in Assignment 8

--- Assignment_07/task_04.dart
+++ Assignment_07/task_04.dart
@@ -1,14 +1,10 @@
 // checking palindrome
-void main(){
-  String palin = "radar";
-
-  for(int i =1; i < palin.length; i++){
-    for(int j = i-1; j < palin.length; j++)
-  {
-    if(palin[i] == palin[j]){
-      print("It's a palindrome");
-    }
+void main() {
+  String str = "mariam";
+  String reversedStr = str.split("").reversed.join("");
+  if (str == reversedStr) {
+    print("$str is a palinadrome.");
+  } else {
+    print("$str is not a palinadrome.");
   }
-  }
-
 }
--- Assignment_08/task_1.dart
+++ Assignment_08/task_1.dart
@@ -1,3 +1,15 @@
+// Q1. Write a Dart program that counts the number of digits in a given number using a while loop.
+
+import 'dart:io';
 void main(){
-  
+  print("Enter a series of numbers for which you want to count the total digits in series");
+  int num = int.parse(stdin.readLineSync()!);
+  int count = 0;
+
+  while (num != 0) {
+    num = num ~/ 10;
+    count++;
+  }
+    print(count);
+
 }
