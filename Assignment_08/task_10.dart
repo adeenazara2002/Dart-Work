079d1bbf626ad14bdb1a9a143a825898ab643b94
adeenazara2002 <adinazara2001@gmail.com>
2024-05-04 22:14:51 +0500

    Adding Task 10

--- Assignment_08/task_10.dart
+++ Assignment_08/task_10.dart
@@ -1,15 +1,23 @@
-// Q10. Create Dart code to calculate the average of numbers in a list using a do-while loop.X
+// Q10. Create Dart code to calculate the average of numbers in a list using a do-while loop.
+import 'dart:io';
 void main(){
-  List<int> nums = [1,2,3,4,5];
-  int count;
+  List<int> nums = [];
+  int count =0;
   int sum = 0;
-  int average = 0;
+  double average = 0;
+  int a =0;
+  print("Enter the list of numbers to calculate sum and average");
+  for(int i =1; i <= 10; i++){
+    nums.add(int.parse(stdin.readLineSync()!));
+  }
+  print("List: ${nums}");
  do {
- print(a);
-  sum = sum+a;
+//  print(nums[a]);
+  sum = sum + nums[a];
+  count++;
  a++;
- }while (a=nums.length);
- print(sum);
-
- 
+ }while (a < nums.length);
+ average = sum / count;
+ print("Sum of the numbers given in list ${sum}");
+ print("Average: ${average}");
 }
