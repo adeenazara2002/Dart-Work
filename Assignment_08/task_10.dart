e94cd678745b78f16e8e5fa307388a6e4bbb8858
adeenazara2002 <adinazara2001@gmail.com>
2024-05-04 21:23:55 +0500

    Adding tasks

---
+++ Assignment_08/task_10.dart
@@ -0,0 +1,15 @@
+// Q10. Create Dart code to calculate the average of numbers in a list using a do-while loop.#
+void main(){
+  List<int> nums = [1,2,3,4,5];
+  int count;
+  int sum = 0;
+  int average = 0;
+ do {
+ print(a);
+  sum = sum+a;
+ a++;
+ }while (a=nums.length);
+ print(sum);
+
+ 
+}
---
+++ Assignment_08/task_12.dart
@@ -0,0 +1,12 @@
+// Q12. Write a Dart program to print even numbers from 1 to 20 using a do-while 
+void main(){
+  int a =1;
+  print("Even Numbers");
+  do {
+    if(a % 2 == 0){
+       print(a);
+
+    }
+   a++;
+ }while (a<21);
+}
