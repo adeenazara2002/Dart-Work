ec01f30ac74f727e8d707d1a098f06d855cd2758
adeenazara2002 <adinazara2001@gmail.com>
2024-05-04 20:54:10 +0500

    Adding Task 8

---
+++ Assignment_08/task_7.dart
@@ -0,0 +1,9 @@
+// Q7. Create a Dart program that prints the first 10 prime numbers using a for loop.
+void main(){
+  int num = 0;
+for(int i = 1; i<= 10; i++){
+  if(i / 1 == 0  || i / i == 0){
+    print(i);
+  }
+}
+}
