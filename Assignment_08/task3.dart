4ad91436263884793f60651f6c1c8eca0cf0df1a
adeenazara2002 <adinazara2001@gmail.com>
2024-05-04 17:25:39 +0500

    Adding Task 3

---
+++ Assignment_08/task3.dart
@@ -0,0 +1,11 @@
+// Q3. Create a Dart program that checks if a given string is empty or not using if-else statements.
+void main(){
+  String name = "";
+  if(name == ""){
+    print("String is empty");
+
+  }
+  else{
+    print("Name: ${name}");
+  }
+}
--- Assignment_08/task_2.dart
+++ Assignment_08/task_2.dart
@@ -3,12 +3,14 @@
 import 'dart:io';
 void main(){
   int n =1;
+  var str = 0;
+  var randomPass;
+  int passLength = 1;
   bool passWord = true;
+  
   print("Enter User name");
   String? userName = stdin.readLineSync();
   print("Username set successfully");
-
-
 
   print("Create Password or generate password? Type 1 for creation of password or type 2 for generation of password");
   int reply = int.parse(stdin.readLineSync()!);
@@ -16,19 +18,25 @@ void main(){
     print("Enter Password");
     var setPass = stdin.readLineSync();
     print("Password set successfuly");
+    print("Account Created!");
+
   }
 
   else if(reply == 2){
-    print("Generating Password");
     while(n != 0){
+  
+      // print("Generating Password");
+      print(n);
+      // String? generatePass = stdin.readLineSync();
+      // var gen = randomPass + 1;
+      if(n == 10){
+        print("Your new password");
+        break;
+      }
+      n++;
       
+
     }
-  }
-
-  var pass = stdin.readLineSync();
-
-  if(passWord){
-    print("Password Set successfully");
   }
 
   
