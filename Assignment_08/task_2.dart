24ca671b01f7845f1a174097c0e959e52912235e
adeenazara2002 <adinazara2001@gmail.com>
2024-05-04 22:58:14 +0500

    Updating Changes

--- Assignment_08/task_2.dart
+++ Assignment_08/task_2.dart
@@ -1,43 +1,44 @@
-// Q2. Implement Dart code to generate a random password of a given length using a while loop.
-// Account creation scenario
 import 'dart:io';
-void main(){
-  int n =1;
-  var str = 0;
-  var randomPass;
-  int passLength = 1;
-  bool passWord = true;
-  
-  print("Enter User name");
+import 'dart:math';
+
+void main() {
+  print("Enter Username:");
   String? userName = stdin.readLineSync();
   print("Username set successfully");
 
-  print("Create Password or generate password? Type 1 for creation of password or type 2 for generation of password");
+  print("Create Password or generate password?");
+  print("Type '1' for creating a password or '2' for generating a password:");
   int reply = int.parse(stdin.readLineSync()!);
-  if(reply == 1){
-    print("Enter Password");
+
+  if (reply == 1) {
+    print("Enter Password:");
     var setPass = stdin.readLineSync();
-    print("Password set successfuly");
+    print("Password set successfully");
     print("Account Created!");
-
+  } else if (reply == 2) {
+    print("Enter the length of the password:");
+    int passLength = int.parse(stdin.readLineSync()!);
+    
+    print("Generating Random Password...");
+    print('Your Generated password is: ${generatePassword(passLength)}');
   }
+}
 
-  else if(reply == 2){
-    while(n != 0){
-  
-      // print("Generating Password");
-      print(n);
-      // String? generatePass = stdin.readLineSync();
-      // var gen = randomPass + 1;
-      if(n == 10){
-        print("Your new password");
-        break;
-      }
-      n++;
-      
+String generatePassword(int length) {
+  String upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  String lower = 'abcdefghijklmnopqrstuvwxyz';
+  String numbers = '1234567890';
+  String symbols = '!@#\$%^&*()<>,./';
+  String seed = upper + lower + numbers + symbols;
+  String password = '';
+  List<String> list = seed.split('');
+  Random rand = Random();
+  int i = 0;
 
-    }
+  while (i < length) {
+    int index = rand.nextInt(list.length);
+    password += list[index];
+    i++;
   }
-
-  
+  return password;
 }
