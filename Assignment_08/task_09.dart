f86a1d9c60c2e93d4dacc1cfba8932d6f481fd58
adeenazara2002 <adinazara2001@gmail.com>
2024-05-04 21:01:01 +0500

    Task 09

---
+++ Assignment_08/task_09.dart
@@ -0,0 +1,11 @@
+// Q9. Implement Dart code to print the squares of numbers from 1 to 5 using a do-while loop.
+void main(){
+   int a = 1;
+   print("Squares of numbers from 1 to 5");
+ do {
+ print("Square of number ${a} is ${a*a}");
+ a++;
+ }while (a<=5);
+
+
+}
